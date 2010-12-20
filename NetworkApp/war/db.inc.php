99e50c8460605b0c27a8403549b53ece3d61d7a0
Georgi Todorov <terahz@geodar.com>
2010-12-20 03:13:53 +0000

    fixed pass variable

--- NetworkApp/war/db.inc.php
+++ NetworkApp/war/db.inc.php
@@ -1,6 +1,6 @@
 <?php 
 define('DB_NAME', 'dbnamem');     // The name of the database
 define('DB_USER', 'dbuser');     // Your MySQL username
-define('DB_PASSWORD', 'dbpass'); // ...and password
+define('DB_PASS', 'dbpass'); // ...and password
 define('DB_HOST', 'localhost');     // hostname
 ?>
